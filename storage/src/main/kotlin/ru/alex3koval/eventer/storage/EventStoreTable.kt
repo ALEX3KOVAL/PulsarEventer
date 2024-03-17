3a60e4ef16684aaa6e661b6b43288a275f0e42e3
Alexandr Kovalenko <destructsymp@gmail.com>
2024-03-18 00:36:40 +0300

    feature/fillEventRDTO: дополнил представление таблицы.

--- storage/src/main/kotlin/ru/alex3koval/eventer/storage/EventStoreTable.kt
+++ storage/src/main/kotlin/ru/alex3koval/eventer/storage/EventStoreTable.kt
@@ -4,8 +4,14 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.javatime.datetime
 
 object EventStoreTable : IntIdTable("event_store") {
+    val aggregateId = varchar("aggregate_id", 255)
     val status = integer("status")
-    val payload = jsonb<String>("payload")
+    val jsonPayload = jsonb<String>("payload")
     val hash = varchar("hash", 255)
+    val type = varchar("type", 255)
+    val producer = varchar("producer", 255)
+    val comment = text("comment").nullable()
+    val topic = varchar("topic", 255).nullable()
+    val createdAt = datetime("created_at")
     val updatedAt = datetime("updated_at")
 }
