f53aa80bd59b63c593d9a85906440813b3cf8885
Alexandr Kovalenko <destructsymp@gmail.com>
2024-03-18 00:32:10 +0300

    feature/fillEventRDTO: удалил неиспользуемый метод.

--- app/src/main/kotlin/ru/alex3koval/eventer/app/contract/EventPersister.kt
+++ app/src/main/kotlin/ru/alex3koval/eventer/app/contract/EventPersister.kt
@@ -1,11 +1,8 @@
 package ru.alex3koval.eventer.app.contract
 
-import ru.alex3koval.eventer.app.persister.CreateEventDTO
 import ru.alex3koval.eventer.app.vo.EventStatus
 import java.time.LocalDateTime
 
 interface EventPersister {
-    fun add(dto: CreateEventDTO): Result<Unit>
-
     fun updateStatus(hash: String, newStatus: EventStatus, updatedAt: LocalDateTime): Result<Unit>
 }
--- storage/src/main/kotlin/ru/alex3koval/eventer/storage/persister/EventPersisterImpl.kt
+++ storage/src/main/kotlin/ru/alex3koval/eventer/storage/persister/EventPersisterImpl.kt
@@ -8,10 +8,6 @@ import ru.alex3koval.eventer.storage.EventStoreTable
 import java.time.LocalDateTime
 
 class EventPersisterImpl : EventPersister {
-    override fun add(dto: CreateEventDTO): Result<Unit> {
-        TODO("Not yet implemented")
-    }
-
     override fun updateStatus(hash: String, newStatus: EventStatus, updatedAt: LocalDateTime): Result<Unit> =
         EventStoreTable.singleUpdate(where = { EventStoreTable.hash eq hash }) {
             it[this.status] = newStatus.value
